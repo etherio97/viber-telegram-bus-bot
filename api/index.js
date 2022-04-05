145409b906c9149449021840a6f914704430b857
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2022-04-05 13:03:24 +0630

    a

--- api/index.js
+++ api/index.js
@@ -77,7 +77,7 @@ app.post('/api', json(), async (req, res) => {
             if (results.length) {
                 let text = `မှတ်တိုင် ${toBurmeseNumber(results.length)} ခု ရှာတွေ့ပါတယ်။\n\n`;
 
-                text += results.map(({ name, distance }, i) => `${toBurmeseNumber(1 + i)}။ ${name} _မီတာ ${toBurmeseNumber(distance.toFixed(0))} အကွာ_`).join('\n');
+                text += results.map(({ name, distance }, i) => `${toBurmeseNumber(1 + i)}။ *${name}*   _မီတာ ${toBurmeseNumber(distance.toFixed(0))} အကွာ_`).join('\n');
 
                 await sendMessage(user.id, {
                     text,
