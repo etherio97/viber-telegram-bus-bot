cf06c6a926f7a97cdbb28c2d4951c50ac336c6db
etherio97 <ethereal97@gmail.com>
2022-04-30 16:08:25 +0630

    c

--- api/messenger.js
+++ api/messenger.js
@@ -13,12 +13,19 @@ app.get("/messenger/webhook", (req, res) => {
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
   if (!(mode && token)) {
-    return res.status(400);
+    res.status(400);
+    res.send('required: mode, token');
+    res.end();
+    return;
   }
   if (!(mode === 'subscribe' && token === 'secret')) {
-    return res.setatus(403);
+    res.status(403);
+    res.send('invalid mode pr token');
+    res.end();
+    return;
   }
   res.status(200).send(challenge);
+  res.end();
 });
 
 app.post('/messenger/webhook', express.json(), async (req, res) => {
