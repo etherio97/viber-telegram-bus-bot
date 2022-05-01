da477a76b3abbc6dd1427e3a696dbcfa0b399a26
etherio97 <ethereal97@gmail.com>
2022-05-01 23:15:00 +0630

    api

--- api/v1/yangon-bus.js
+++ api/v1/yangon-bus.js
@@ -1,20 +1,48 @@
 const {
-  app, 
+  app,
   createRouter,
 } = require('../../src/app');
 
 const {
-  toBurmeseNumber,
   calculateDistance,
   getRadius,
 } = require('../../src/utils');
 
-const { 
-  LINE_TYPES, 
-  findLinesByStop, 
+const {
+  LINE_TYPES,
+  findLinesByStop,
   findNearestStops,
 } = require('../../src/bus');
 
 const router = createRouter('/v1/yangon-bus');
+
+router.get('/near-me', async (req, res) => {
+  let { latitude, longitude, radius } = req.query;
+  if (!(latitude && longitude)) {
+    return res.status(404).send('required arguments: latitude, longitude');
+  }
+  if (radius && radius > 5000) {
+    return res.status(404).send('invalid argument: radius must be under 5000 meters');
+  }
+  let params = getRadius(radius || 1000, { latitude, longitude });
+  let results = (await findNearestStops(params))
+    .map((result) => ({
+      ...result,
+      distance: calculateDistance(result, { latitude, longitude }),
+    }))
+    .sort((a, b) => a.distance - b.distance);
+  res.json(results);
+});
+
+router.get('/:id/lines', async (req, res) => {
+  let { id } = req.params;
+  let results = (await findLinesByStop(id))
+    .map((value) => ({
+      ...value,
+      line: LINE_TYPES[value.line_type],
+      line_type: undefined,
+    }));
+  res.json(results);
+})
 
 module.exports = app;
